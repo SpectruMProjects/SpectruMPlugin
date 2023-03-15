3762c8d2149f59386d877b1017fd0e7656615dd0
Gleb <M20XV@yandex.ru>
2023-03-15 23:22:58 +0300

    replace coroutine dispatcher from ThreadPool to Bukkit Scheduler

--- src/main/java/spectrum/sexplugin/SexPlugin.kt
+++ src/main/java/spectrum/sexplugin/SexPlugin.kt
@@ -2,42 +2,45 @@ package spectrum.sexplugin
 
 import kotlinx.coroutines.*
 import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 import spectrum.sexplugin.menu.MenuModule
 import spectrum.sexplugin.particles.ParticlesModule
-import java.util.concurrent.Executors
-import kotlin.time.Duration
+import kotlin.coroutines.CoroutineContext
 
 class SexPlugin : JavaPlugin() {
     companion object {
-        private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
-        private lateinit var mainScope: CoroutineScope
+        lateinit var MainDispatcher: MainCoroutineDispatcher
+        private set
 
-        lateinit var coroutineScope: CoroutineScope
+        lateinit var DefaultDispatcher: CoroutineDispatcher
+        private set
+
+        lateinit var mainScope: CoroutineScope
+        private set
+
+        lateinit var defaultScope: CoroutineScope
         private set
 
         lateinit var plugin: SexPlugin
-        private set
+            private set
+
     }
 
     override fun onEnable() {
         plugin = this
-        CoroutineScope(dispatcher).launch {
-            mainScope = this
-            supervisorScope {
-                coroutineScope = this@supervisorScope
-                delay(Duration.INFINITE)
-            }
-        }
-
+        MainDispatcher = MainThreadDispatcher(this)
+        DefaultDispatcher = PluginDispatcher(this)
+        mainScope = CoroutineScope(MainDispatcher) + SupervisorJob()
+        defaultScope = CoroutineScope(DefaultDispatcher) + SupervisorJob()
         registerEvents()
         init()
     }
 
     override fun onDisable() {
-        mainScope.cancel()
-        dispatcher.cancel()
-        dispatcher.close()
+        MainDispatcher.cancel()
+        DefaultDispatcher.cancel()
         Thread.sleep(100)
     }
 
@@ -51,5 +54,29 @@ class SexPlugin : JavaPlugin() {
 
     private fun init() {
         ParticlesModule.onInit(this)
+    }
+}
+
+inline fun task(crossinline block: () -> Unit) = object : BukkitRunnable() {
+    override fun run() {
+        block()
+    }
+}
+
+class MainThreadDispatcher(
+    private val plugin: Plugin
+): MainCoroutineDispatcher() {
+    override val immediate: MainCoroutineDispatcher = this
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        task { block.run() }.runTask(plugin)
+    }
+
+}
+class PluginDispatcher(
+    private val plugin: Plugin
+): CoroutineDispatcher() {
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        task { block.run() }.runTaskAsynchronously(plugin)
     }
 }
