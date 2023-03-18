0d53cd4c5f02ae9dccbaf9b22db3adc99a15bd39
Савелій Савёнак <1986developer@gmail.com>
2023-03-18 12:25:13 +0300

    Fake to Api

--- src/main/java/spectrum/sexplugin/whitelist/WhitelistModule.kt
+++ src/main/java/spectrum/sexplugin/whitelist/WhitelistModule.kt
@@ -7,7 +7,7 @@ import spectrum.sexplugin.whitelist.services.WhitelistService
 object WhitelistModule {
     fun init(plugin: SexPlugin) {
         val api = Api("localhost", 5168)
-        val service = WhitelistService.Fake()
+        val service = WhitelistService.Api(api)
         val listener = JoinListener(service)
 
         plugin.registerEventListener(listener)
