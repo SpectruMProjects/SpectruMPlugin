4e5c0269518dbce5b35e1ca46bdcdf5607032d7f
Gleb <M20XV@yandex.ru>
2023-03-18 22:13:50 +0300

    remove actors from master

--- src/main/java/spectrum/sexplugin/hardcore/HardcoreModule.kt
+++ src/main/java/spectrum/sexplugin/hardcore/HardcoreModule.kt
@@ -1,21 +1,20 @@
 package spectrum.sexplugin.hardcore
 
-import kotlinx.coroutines.launch
 import spectrum.sexplugin.SexPlugin
-import spectrum.sexplugin.hardcore.commands.StatisticsServiceRestartCommand
 import spectrum.sexplugin.hardcore.models.Statistics
+import spectrum.sexplugin.hardcore.repos.MemoryStatisticsRepository
 import spectrum.sexplugin.hardcore.repos.MongoStatisticsRepository
 import spectrum.sexplugin.hardcore.services.StatisticsService
-import spectrum.sexplugin.hardcore.services.StatisticsServiceActor
 
 object HardcoreModule {
-    lateinit var service: StatisticsServiceActor
+    lateinit var service: StatisticsService
     private set
 
     fun init(plugin: SexPlugin) {
-        service = StatisticsServiceActor()
-
-        plugin.getCommand("hardcore-stats-service-restart")
-            ?.setExecutor(StatisticsServiceRestartCommand(service))
+        val repo =
+            if (SexPlugin.plugin.config.getString("hardcore-services-statistics-repo_type", "memory") == "memory")
+                MemoryStatisticsRepository()
+            else MongoStatisticsRepository(SexPlugin.plugin.db.getCollection("statistics", Statistics::class.java))
+        service = StatisticsService(repo)
     }
 }
