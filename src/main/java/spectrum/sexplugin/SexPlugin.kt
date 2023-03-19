06ed775c6add09eee14f8b0256503205afafe632
Савелій Савёнак <1986developer@gmail.com>
2023-03-19 11:48:22 +0300

    Fixes, shit

--- src/main/java/spectrum/sexplugin/SexPlugin.kt
+++ src/main/java/spectrum/sexplugin/SexPlugin.kt
@@ -4,43 +4,38 @@ import kotlinx.coroutines.*
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
-import org.bukkit.plugin.PluginLogger
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 import spectrum.sexplugin.menu.MenuModule
 import spectrum.sexplugin.whitelist.WhitelistModule
+import java.util.logging.Logger
 import kotlin.coroutines.CoroutineContext
 
 class SexPlugin : JavaPlugin() {
     companion object {
         lateinit var MainDispatcher: MainCoroutineDispatcher
         private set
-        lateinit var Config: FileConfiguration;
         lateinit var DefaultDispatcher: CoroutineDispatcher
         private set
-
         lateinit var mainScope: CoroutineScope
         private set
-
         lateinit var defaultScope: CoroutineScope
         private set
-
-        lateinit var plugin: SexPlugin
+        lateinit var plugin: JavaPlugin
         private set
-
-        lateinit var logger: PluginLogger
+        lateinit var Config: FileConfiguration
         private set
+        lateinit var mainlogger: Logger
     }
 
     override fun onEnable() {
-       // this.config.addDefault("whitelist-active", true)
-        this.saveDefaultConfig()
-        Config = this.config
         plugin = this
+        mainlogger = plugin.getLogger()
         MainDispatcher = MainThreadDispatcher(this)
         DefaultDispatcher = PluginDispatcher(this)
         mainScope = CoroutineScope(MainDispatcher) + SupervisorJob()
         defaultScope = CoroutineScope(DefaultDispatcher) + SupervisorJob()
+        initConfig()
         registerEvents()
         init()
     }
@@ -51,6 +46,17 @@ class SexPlugin : JavaPlugin() {
         Thread.sleep(100)
     }
 
+    private fun initConfig()
+    {
+        saveDefaultConfig()
+        Config = config
+//        val file = File(dataFolder.name + File.pathSeparator + "config.yml")
+//        if(!file.exists())
+//        {
+//            saveResource("config.yml", true)
+//        }
+//        Config = YamlConfiguration.loadConfiguration(file)
+    }
     fun registerEventListener(listener: Listener) {
         server.pluginManager.registerEvents(listener, this)
     }
