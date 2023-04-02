bc902bc9597ad5cfc59340cd086ade023929b3f7
Савелій Савёнак <1986developer@gmail.com>
2023-04-02 21:57:43 +0300

    I think that's work??? Maybe... Stashing

--- src/main/java/spectrum/sexplugin/hardcore/HardcoreListener.kt
+++ src/main/java/spectrum/sexplugin/hardcore/HardcoreListener.kt
@@ -1,9 +1,18 @@
 package spectrum.sexplugin.hardcore
 
+import com.mongodb.client.model.Filters.eq
 import org.bson.types.ObjectId
+import org.bukkit.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import org.litote.kmongo.MongoOperator
+import org.litote.kmongo.find
+import org.litote.kmongo.findOne
+import org.litote.kmongo.replaceOneById
+import spectrum.sexplugin.SexPlugin
 import spectrum.sexplugin.hardcore.models.Stat
 import spectrum.sexplugin.hardcore.models.UserStatistics
 import java.sql.Time
@@ -12,11 +21,57 @@ import java.time.LocalTime
 class HardcoreListener : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent){
-        Mongo.UserStatistics.insertOne(
-            UserStatistics(
-                ObjectId.get(),
-                event.player.name,
-                LocalTime.MIN,
-                emptyList<Stat>().toMutableList()))
+        var user = Mongo.UserStatistics.findOne(eq("username", event.player.name))
+        if(user == null) {
+            SexPlugin.plugin.logger.warning("${event.player.name} not found, creating")
+            Mongo.UserStatistics.insertOne(
+                UserStatistics(
+                    ObjectId.get(),
+                    event.player.name,
+                    0,
+                    System.currentTimeMillis(),
+                    emptyList<Stat>().toMutableList()
+                )
+            )
+        }else{
+            user = updateLastServerTime(user)
+            Mongo.UserStatistics.replaceOneById(user._id, user)
+        }
+    }
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent)
+    {
+        SexPlugin.plugin.logger.warning("player dead ${event.player.name}")
+        var user = Mongo.UserStatistics.findOne(eq("username",event.player.name))
+        if(user == null)
+        {
+            event.player.gameMode = GameMode.SURVIVAL
+            event.player.spawnAt(Location(Bukkit.getWorld(event.player.name),0.0,80.0, 0.0))
+            return
+        }
+        user = updateTimeOnServer(user)
+        user = updateLastServerTime(user)
+        user.stats.add(Stat(user.timeOnServer / 2, event.deathMessage!!))
+        Mongo.UserStatistics.replaceOneById(user._id, user)
+    }
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent)
+    {
+        var user = Mongo.UserStatistics.findOne(eq("username",event.player.name))
+        if(event.player.gameMode == GameMode.SPECTATOR){
+            user = updateLastServerTime(user!!)
+            Mongo.UserStatistics.replaceOneById(user._id, user)
+        }
+        user = updateTimeOnServer(user!!)
+        user = updateLastServerTime(user)
+        Mongo.UserStatistics.replaceOneById(user._id, user)
+    }
+    private fun updateTimeOnServer(user: UserStatistics): UserStatistics
+    {
+        return user.copy(timeOnServer = user.timeOnServer + System.currentTimeMillis() - user.lastServerTime)
+    }
+    private fun updateLastServerTime(user: UserStatistics): UserStatistics
+    {
+        return user.copy(lastServerTime = System.currentTimeMillis())
     }
 }
--- src/main/java/spectrum/sexplugin/hardcore/models/UserStatistics.kt
+++ src/main/java/spectrum/sexplugin/hardcore/models/UserStatistics.kt
@@ -4,5 +4,5 @@ import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 import java.time.LocalTime
 
-data class UserStatistics(@BsonId val _id: ObjectId, val userid: String, val timeOnServer: LocalTime, val stats: MutableList<Stat>)
-data class Stat(val timeToRespawn: LocalTime, val deathIssue: String)
+data class UserStatistics(@BsonId val _id: ObjectId, val username: String, val timeOnServer: Long, val lastServerTime: Long, val stats: MutableList<Stat>)
+data class Stat(val timeToRespawn: Long, val deathIssue: String)
