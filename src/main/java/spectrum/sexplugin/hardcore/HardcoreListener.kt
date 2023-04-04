24b320ec7bf53d1990095bdecefabe4c3f01e3c6
Савелій Савёнак <1986developer@gmail.com>
2023-04-04 03:09:45 +0300

    Some shit. На самом деле логика вся написана. Возможно(скорее всего) здесь есть баги, надо отладить.

--- src/main/java/spectrum/sexplugin/hardcore/HardcoreListener.kt
+++ src/main/java/spectrum/sexplugin/hardcore/HardcoreListener.kt
@@ -1,8 +1,10 @@
 package spectrum.sexplugin.hardcore
 
 import com.mongodb.client.model.Filters.eq
+import net.kyori.adventure.text.Component
 import org.bson.types.ObjectId
 import org.bukkit.*
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
@@ -30,6 +32,14 @@ class HardcoreListener : Listener {
                 )
             )
         }else{
+            if(System.currentTimeMillis() > user.stats.last().timeToRespawn && user.lastServerTime < user.stats.last().timeToRespawn)
+            {
+                spawnPlayer(event.player)
+            }
+            if(System.currentTimeMillis() < user.stats.last().timeToRespawn)
+            {
+                respawnTask(user, event.player)
+            }
             user = updateLastServerTime(user)
             Mongo.UserStatistics.replaceOneById(user._id, user)
         }
@@ -48,20 +58,21 @@ class HardcoreListener : Listener {
         user = updateTimeOnServer(user)
         user = updateLastServerTime(user)
         @Suppress("DEPRECATION")
-        user.stats.add(Stat(user.timeOnServer / 2, event.deathMessage!!))
+        user.stats.add(Stat(user.timeOnServer / 2 + System.currentTimeMillis(), event.deathMessage!!))
         Mongo.UserStatistics.replaceOneById(user._id, user)
+        //Game Logic
+        respawnTask(user, event.player)
     }
     @EventHandler
     fun onPlayerQuit(event: PlayerQuitEvent)
     {
         var user = Mongo.UserStatistics.findOne(eq("username",event.player.name))
         if(event.player.gameMode == GameMode.SPECTATOR || event.player.isDead){
-            user = updateLastServerTime(user!!)
-            Mongo.UserStatistics.replaceOneById(user._id, user)
+            return
         }
-        user = updateTimeOnServer(user!!)
-        user = updateLastServerTime(user)
-        Mongo.UserStatistics.replaceOneById(user._id, user)
+            user = updateTimeOnServer(user!!)
+            user = updateLastServerTime(user)
+            Mongo.UserStatistics.replaceOneById(user._id, user)
     }
     private fun updateTimeOnServer(user: UserStatistics): UserStatistics
     {
@@ -70,5 +81,26 @@ class HardcoreListener : Listener {
     private fun updateLastServerTime(user: UserStatistics): UserStatistics
     {
         return user.copy(lastServerTime = System.currentTimeMillis())
+    }
+    private fun spawnPlayer(player: Player)
+    {
+        player.teleport(Bukkit.getWorld("World")!!.spawnLocation)
+        player.gameMode = GameMode.SURVIVAL
+    }
+    private fun respawnTask(user: UserStatistics, player: Player){
+        Bukkit.getScheduler().runTaskAsynchronously(SexPlugin.plugin, Runnable{
+            while(System.currentTimeMillis() < user.stats.last().timeToRespawn) {
+                Thread.sleep(100)
+                //SexPlugin.plugin.logger.warning((System.currentTimeMillis() - user.stats.last().timeToRespawn).toString())
+                try {
+                    player.sendMessage(Component.text(user.stats.last().timeToRespawn - System.currentTimeMillis()))
+                }catch (_: Exception) {}
+            }
+            Bukkit.getScheduler().runTask(SexPlugin.plugin, Runnable {
+                try {
+                    spawnPlayer(player)
+                }catch (_: Exception){}
+            })
+        })
     }
 }
--- src/main/java/spectrum/sexplugin/hardcore/HardcoreModule.kt
+++ src/main/java/spectrum/sexplugin/hardcore/HardcoreModule.kt
@@ -1,6 +1,8 @@
 package spectrum.sexplugin.hardcore
 
+import org.bukkit.Bukkit
 import spectrum.sexplugin.SexPlugin
+import spectrum.sexplugin.hardcore.commands.ResetTimeHandler
 import spectrum.sexplugin.hardcore.models.DatabaseData
 
 object HardcoreModule {
@@ -12,7 +14,12 @@ object HardcoreModule {
             plugin.config.getString("mongo-db")!!,
             "hardcore-stats"
         )
+        if(!Bukkit.isHardcore()) {
+            plugin.logger.warning("Server is not on hardcore mode! Hardcore module will be disabled")
+            return
+        }
         Mongo.initMongoCollection(data)
         plugin.registerEventListener(HardcoreListener())
+        plugin.getCommand("resettime")!!.setExecutor(ResetTimeHandler())
     }
 }
