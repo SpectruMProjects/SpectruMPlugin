19014d250eb13b4d4530b49c428571a484271e0b
Савелій Савёнак <1986developer@gmail.com>
2023-04-04 15:13:30 +0300

    Added bossBar, config.yml changed, added showstats command

--- src/main/java/spectrum/sexplugin/hardcore/HardcoreListener.kt
+++ src/main/java/spectrum/sexplugin/hardcore/HardcoreListener.kt
@@ -1,7 +1,9 @@
 package spectrum.sexplugin.hardcore
 
 import com.mongodb.client.model.Filters.eq
+import kotlinx.coroutines.launch
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.Title
 import org.bson.types.ObjectId
 import org.bukkit.*
 import org.bukkit.entity.Player
@@ -15,92 +17,129 @@ import org.litote.kmongo.replaceOneById
 import spectrum.sexplugin.SexPlugin
 import spectrum.sexplugin.hardcore.models.Stat
 import spectrum.sexplugin.hardcore.models.UserStatistics
+import java.time.Duration
 
 class HardcoreListener : Listener {
     @EventHandler
-    fun onPlayerJoin(event: PlayerJoinEvent){
+    fun onPlayerJoin(event: PlayerJoinEvent) {
         var user = Mongo.UserStatistics.findOne(eq("username", event.player.name))
-        if(user == null) {
+        if (user == null) {
             SexPlugin.plugin.logger.warning("${event.player.name} not found, creating")
             Mongo.UserStatistics.insertOne(
                 UserStatistics(
-                    ObjectId.get(),
-                    event.player.name,
-                    0,
-                    System.currentTimeMillis(),
-                    emptyList<Stat>().toMutableList()
+                    ObjectId.get(), event.player.name, 0, System.currentTimeMillis(), emptyList<Stat>().toMutableList()
                 )
             )
-        }else{
-            if(System.currentTimeMillis() > user.stats.last().timeToRespawn && user.lastServerTime < user.stats.last().timeToRespawn)
-            {
+        } else {
+            if (System.currentTimeMillis() > user.stats.last().timeToRespawn && user.lastServerTime < user.stats.last().timeToRespawn) {
                 spawnPlayer(event.player)
             }
-            if(System.currentTimeMillis() < user.stats.last().timeToRespawn)
-            {
+            if (System.currentTimeMillis() < user.stats.last().timeToRespawn) {
                 respawnTask(user, event.player)
             }
             user = updateLastServerTime(user)
             Mongo.UserStatistics.replaceOneById(user._id, user)
         }
     }
+
     @EventHandler
-    fun onPlayerDeath(event: PlayerDeathEvent)
-    {
+    fun onPlayerDeath(event: PlayerDeathEvent) {
         SexPlugin.plugin.logger.warning("player dead ${event.player.name}")
-        var user = Mongo.UserStatistics.findOne(eq("username",event.player.name))
-        if(user == null)
-        {
+        var user = Mongo.UserStatistics.findOne(eq("username", event.player.name))
+        if (user == null) {
             event.player.gameMode = GameMode.SURVIVAL
-            event.player.spawnAt(Location(Bukkit.getWorld(event.player.name),0.0,80.0, 0.0))
+            event.player.spawnAt(Location(Bukkit.getWorld(event.player.name), 0.0, 80.0, 0.0))
             return
+        }
+        if(user.stats.size > 0) {
+            if (System.currentTimeMillis() < user.stats.last().timeToRespawn) {
+                return
+            }
         }
         user = updateTimeOnServer(user)
         user = updateLastServerTime(user)
-        @Suppress("DEPRECATION")
-        user.stats.add(Stat(user.timeOnServer / 2 + System.currentTimeMillis(), event.deathMessage!!))
+        @Suppress("DEPRECATION") user.stats.add(
+            Stat(
+                user.timeOnServer / 2 + System.currentTimeMillis(),
+                event.deathMessage!!
+            )
+        )
         Mongo.UserStatistics.replaceOneById(user._id, user)
         //Game Logic
         respawnTask(user, event.player)
     }
+
     @EventHandler
-    fun onPlayerQuit(event: PlayerQuitEvent)
-    {
-        var user = Mongo.UserStatistics.findOne(eq("username",event.player.name))
-        if(event.player.gameMode == GameMode.SPECTATOR || event.player.isDead){
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        var user = Mongo.UserStatistics.findOne(eq("username", event.player.name))
+        if (event.player.gameMode == GameMode.SPECTATOR || event.player.isDead) {
             return
         }
-            user = updateTimeOnServer(user!!)
-            user = updateLastServerTime(user)
-            Mongo.UserStatistics.replaceOneById(user._id, user)
+        user = updateTimeOnServer(user!!)
+        user = updateLastServerTime(user)
+        Mongo.UserStatistics.replaceOneById(user._id, user)
     }
-    private fun updateTimeOnServer(user: UserStatistics): UserStatistics
-    {
+
+    private fun updateTimeOnServer(user: UserStatistics): UserStatistics {
         return user.copy(timeOnServer = user.timeOnServer + System.currentTimeMillis() - user.lastServerTime)
     }
-    private fun updateLastServerTime(user: UserStatistics): UserStatistics
-    {
+
+    private fun updateLastServerTime(user: UserStatistics): UserStatistics {
         return user.copy(lastServerTime = System.currentTimeMillis())
     }
-    private fun spawnPlayer(player: Player)
-    {
+
+    private fun spawnPlayer(player: Player) {
         player.teleport(Bukkit.getWorld("World")!!.spawnLocation)
         player.gameMode = GameMode.SURVIVAL
     }
-    private fun respawnTask(user: UserStatistics, player: Player){
-        Bukkit.getScheduler().runTaskAsynchronously(SexPlugin.plugin, Runnable{
-            while(System.currentTimeMillis() < user.stats.last().timeToRespawn) {
+
+    private fun respawnTask(user: UserStatistics, player: Player) {
+        SexPlugin.defaultScope.launch {
+            var message = false
+            val startTime = System.currentTimeMillis()
+            var bar = net.kyori.adventure.bossbar.BossBar.bossBar(
+                Component.text("До возрождения: " + ( user.stats.last().timeToRespawn - System.currentTimeMillis())),
+                1.0f,
+                net.kyori.adventure.bossbar.BossBar.Color.GREEN,
+                net.kyori.adventure.bossbar.BossBar.Overlay.PROGRESS
+            )
+            while (System.currentTimeMillis() < user.stats.last().timeToRespawn) {
                 Thread.sleep(100)
-                //SexPlugin.plugin.logger.warning((System.currentTimeMillis() - user.stats.last().timeToRespawn).toString())
+                player.sendMessage(((user.stats.last().timeToRespawn - startTime) / (user.stats.last().timeToRespawn - System.currentTimeMillis())).toFloat().toString())
                 try {
-                    player.sendMessage(Component.text(user.stats.last().timeToRespawn - System.currentTimeMillis()))
-                }catch (_: Exception) {}
+                    if (!player.isDead) {
+                        //Show title
+                        if (!message) {
+                            val title = Title.title(
+                                Component.text(HardcoreModule.hardcoreConfig.getString("title")!!),
+                                Component.text(HardcoreModule.hardcoreConfig.getString("subtitle")!!),
+                                Title.Times.times(Duration.ofSeconds(3), Duration.ofSeconds(5), Duration.ofSeconds(3))
+                            )
+                            player.showTitle(title)
+                            message = true
+                        }
+                        player.hideBossBar(bar)
+                        val newbar = net.kyori.adventure.bossbar.BossBar.bossBar(
+                            Component.text("До возрождения: " + (user.stats.last().timeToRespawn - System.currentTimeMillis())),
+                            1.0f,
+                            net.kyori.adventure.bossbar.BossBar.Color.GREEN,
+                            net.kyori.adventure.bossbar.BossBar.Overlay.PROGRESS
+                        )
+                        player.showBossBar(newbar)
+                        bar = newbar
+                    }
+                } catch (_: Exception) {
+                    SexPlugin.plugin.logger.warning("Respawn task ended but player has been left. Aborting...")
+                    return@launch
+                }
             }
-            Bukkit.getScheduler().runTask(SexPlugin.plugin, Runnable {
+            launch(SexPlugin.MainDispatcher) {
                 try {
+                    player.hideBossBar(bar)
                     spawnPlayer(player)
-                }catch (_: Exception){}
-            })
-        })
+                } catch (_: Exception) {
+                }
+            }
+        }
     }
 }
